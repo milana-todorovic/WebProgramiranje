a8c761fd0768c00799ece707c0a8ab7c71d56958
Ana Perisic <anaperisictb@yahoo.com>
2020-09-03 19:30:44 +0200

    Izmijenjen pregled apartmana i dodate kartice za rezervacije

--- WebApartmani/WebContent/app/guest/components/guestApartment.js
+++ WebApartmani/WebContent/app/guest/components/guestApartment.js
@@ -2,21 +2,32 @@ Vue.component("guest-apartments",{
     data: function(){
         return{
             name: 'Apartmani',
-            show: true,
-            dismissSecs: 2,
-            dismissCountDown: 0,
             apartmani:[
                 {
-                    mico:'Apartmani Ivana',
+                ime:'Apartmani Ivana',
                 sadrzaj:'Jastuk,krevet,jastucnica,posudje',
                 brojSoba:5,
-                aktivan:false
+                aktivan:false,
+                tip:'soba',
+                brojOsoba:2,
+                lokacija:'Pariz',
+                cena:'20$',
+                prijava:'14:00h',
+                odjava:'11:00h',
+                komentari:'Apartman za svaku preporuku.Doci cemo ponovo!'
                 },
                 {
-                mico:'Apartmani Mico',
+                ime:'Apartmani Mico',
                 sadrzaj:'Jastuk,krevet,jastucnica,posudje',
                 brojSoba:6,
-                aktivan:true
+                aktivan:true,
+                tip:'ceo apartman',
+                brojOsoba:4,
+                lokacija:'Santorini',
+                cena:'40$',
+                prijava:'14:00h',
+                odjava:'12:00h',
+                komentari:'Udaljeno od centra,ali veoma cisto.'
                 }
             ],
             options: [
@@ -47,82 +58,148 @@ Vue.component("guest-apartments",{
         }
     
     },
-    methods: {
-        countDownChanged(dismissCountDown) {
-            this.dismissCountDown = dismissCountDown
-          },
-        showAlert() {
-            this.dismissCountDown = this.dismissSecs
-          }
-      },
     template:`
     <div>
-        <div id="pretraga">
-            <b-form inline>
-                <b-form-input  placeholder="Lokacija"></b-form-input>
-                <b-form-input   placeholder="Broj osoba"></b-form-input>
-                <b-form-input   placeholder="Broj soba"></b-form-input>
-                <b-form-input   placeholder="Cena"></b-form-input>
-                <b-form-datepicker  placeholder="Pocetni datum"></b-form-datepicker>
-                <b-form-datepicker  placeholder="Krajnji datum"></b-form-datepicker>
-                
-                <b-button pill id="pretrazi" variant="primary">
-                    <b-icon icon="search"></b-icon>
-                    Pretrazi
-                </b-button>
-            </b-form>
-        </div>
+           
+            <b-card id="pretraga">
+                <b-card-text>
+                    <b-form inline>
+                        <b-form-input  placeholder="Lokacija"></b-form-input>
+                        <b-form-input   placeholder="Broj osoba"></b-form-input>
+                        <b-form-input   placeholder="Broj soba"></b-form-input>
+                        <b-form-input   placeholder="Cena po no&#x107;enju"></b-form-input>
+                        <b-form-datepicker  placeholder="Po&#269;etni datum"></b-form-datepicker>
+                        <b-form-datepicker  placeholder="Krajnji datum"></b-form-datepicker>
+                        
+                        <b-button   variant="primary">
+                            <b-icon icon="search"></b-icon>
+                            Pretrazi
+                        </b-button>
+                    </b-form>
+                </b-card-text>
+            </b-card>
+            
+    
     
         <br><br>
         <b-containter>
-            <b-col>
-                <div id="filtriranje">
-                    <b><b-form-group label="Sortiranje po ceni"></b>
-                        <b-form-checkbox-group
-                        :options="options3"
-                        plain
-                        stacked
-                        ></b-form-checkbox-group>
-                    </b-form-group>
-                    <br><br>
-                    <b-button id="filtrirajDugme" pill variant="primary"> 
-                        <b-icon icon="arrow-down-up"></b-icon>
-                        Sortiraj
-                    </b-button>
-                </div>
-                <br>
-           
-                <div id="filtriranje">
-                   
-                    <b><b-form-group label="Tip apartmana"></b>
-                        <b-form-checkbox-group
-                            :options="options"
-                            plain
-                            stacked
-                            ></b-form-checkbox-group>
-                        </b-form-group>
+            <b-row align-v="start">
+                <b-col sm="2">
+                    <div>
+                        <b-card id="filtriranje">
+                            <b><b-form-group label="Sortiranje po ceni"></b>
+                            <b-form-checkbox-group
+                                :options="options3"
+                                plain
+                                stacked
+                                ></b-form-checkbox-group>
+                            </b-form-group>
+                            <br><br>
+                            <b-button   variant="primary"> 
+                                <b-icon icon="arrow-down-up"></b-icon>
+                                Sortiraj
+                            </b-button>
+                        </b-card>
+                        
+                    </div>
+                    <br>
+            
+                    <div>
+                        <b-card id="filtriranje">
+                            <b-card-text>
+                                <b><b-form-group label="Tip apartmana"></b>
+                                <b-form-checkbox-group
+                                    :options="options"
+                                    plain
+                                    stacked
+                                    ></b-form-checkbox-group>
+                                </b-form-group>
 
-                    <br><br>
+                                <br><br>
 
-                    <b><b-form-group label="Sadrzaji"></b>
-                        <b-form-checkbox-group
-                            :options="options2"
-                            plain
-                            stacked
-                            ></b-form-checkbox-group>
-                        </b-form-group>
-                
-                    <br><br>
-
-                    <b-button id="filtrirajDugme"pill variant="primary">
-                        <b-icon icon="funnel-fill"></b-icon>
-                            Filtriraj
-                    </b-button>
+                                <b><b-form-group label="Sadrzaji"></b>
+                                    <b-form-checkbox-group
+                                        :options="options2"
+                                        plain
+                                        stacked
+                                        ></b-form-checkbox-group>
+                                    </b-form-group>
                             
-                </div>
-                
+                                <br><br>
+
+                                <b-button  variant="primary">
+                                    <b-icon icon="funnel-fill"></b-icon>
+                                        Filtriraj
+                                </b-button>
+                                
+                            </b-card-text>
+                        </b-card>
                     
-            </b-col>
+                        
+                    </div>
+                    
+                        
+                </b-col>
+
+                
+
+                <b-col>
+                    <div>
+                        <dl>
+                            <dd v-for="apartman in apartmani">
+                
+                                <b-card style="max-width: 740px;margin-left:10%" no-body>
+                    
+                                    <b-row no-gutters>
+                                        <b-col>
+                                            <b-card-img src="https://picsum.photos/400/400/?image=20" 			alt="Image" class="rounded-0"></b-card-img>
+                                        </b-col>
+                                        
+                                        <b-col>
+                                            <b-card-body>
+                                                <b-card-text>
+                                                    <h1 id="nazivApartmana">
+                                                    {{apartman.ime}}<b-badge variant="success">{{apartman.tip}}</b-badge>
+                                                    </h1>
+
+                                                    
+
+                                                    <br>
+                                                    <b-icon icon="geo-alt" style="width: 25px; height: 25px;">
+                                                    </b-icon>
+
+                                                    <span style="font-size:20px">
+                                                        {{apartman.lokacija}}
+                                                    </span>
+                                                  
+                                                    <br>
+                                                    <span style="font-size:25px">
+                                                      <b>{{apartman.cena}}</b>
+                                                    </span>
+                                                    
+                                                        <i>po no&#x107;enju</i>
+                                                   
+                                                    <br><br><br><br><br>
+                                                    <b-button variant="primary">
+                                                        Prika&#x17E;i detalje
+                                                        <b-icon icon="arrow-right">
+                                                    </b-button>
+                                                   
+                        
+                                    
+                                                </b-card-text>
+                                            </b-card-body>
+                                        </b-col>
+                                    </b-row>
+                                </b-card>
+                                <br><br>
+                        
+                        
+                            </dd>
+                        </dl>
+                    </div>
+                </b-col>
+            </b-row>
 
 
            
--- WebApartmani/WebContent/app/guest/components/guestReservation.js
+++ WebApartmani/WebContent/app/guest/components/guestReservation.js
@@ -1,12 +1,54 @@
 Vue.component("guest-reservations",{
     data: function(){
         return{
+            name:'Rezervacije',
+            rezervacije:[
+                {
+                    imeApartmana:'Apartmani Ivana',
+                    pocetniDatum:'25.5.2020',
+                    krajnjiDatum:'28.10.2020',
+                    cenaApartmana:'400$'
+                },
+                {
+                    imeApartmana:'Apartmani Ana',
+                    pocetniDatum:'2.8.2020',
+                    krajnjiDatum:'28.8.2020',
+                    cenaApartmana:'300$'
+                },
+                {
+                    imeApartmana:'Sobe Slobodan Bajic',
+                    pocetniDatum:'21.10.2020',
+                    krajnjiDatum:'22.10.2020',
+                    cenaApartmana:'40$'
+                }
+            ]
 
         }
     },
     template:`
         <div>
-            <h1>Sad sam u REZERVACIJAMA!</h1>
+            <dl>
+                <dd v-for="rezervacija in rezervacije">
+                <b-card style="max-width: 840px;margin-left:10%;margin-top:5%">
+                    <h1 id="nazivApartmana">
+                    {{rezervacija.imeApartmana}}
+
+                    </h1>
+
+                    
+                    <div style="background-color:teal;width:20%;padding:2%;color:white;margin-top:5%">
+                        <i>Od</i> {{rezervacija.pocetniDatum}}
+                        <i>do</i>{{rezervacija.krajnjiDatum}}
+                        <br>
+                        {{rezervacija.cenaApartmana}}
+                    </div>
+
+                    
+                </b-card>
+                </dd>
+            </dl>
+            
+            
         </div>
     `
 });
