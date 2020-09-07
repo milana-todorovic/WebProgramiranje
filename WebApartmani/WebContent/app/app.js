db6dd3cce584352ea458c963a6b2fc4f3bfa6efd
milana-todorovic <milanatodorovic@uns.ac.rs>
2020-09-07 22:40:08 +0200

    Base64 test - slanje slika na server i nazad u base64 formatu

--- WebApartmani/WebContent/app/app.js
+++ WebApartmani/WebContent/app/app.js
@@ -1,9 +1,29 @@
 var app = new Vue({
 	el : "#test",
 	data : {
-		title : ""
+		files : [],
+		src : "https://www.abc.net.au/cm/rimage/9417180-16x9-xlarge.jpg?v=2"
 	},
-	mounted(){
-		axios.get("rest/test/testMetoda").then(response => (this.title = response.data))
+	methods : {
+		click : function() {
+			if (this.files.length > 0) {
+				var reader = new FileReader();
+				reader.readAsDataURL(this.files[0]);
+				reader.onload = 
+					() => { axios({
+		                method: 'POST',
+		                url: "http://localhost:8081/WebApartmani/rest/image/base64", 
+		                data: JSON.stringify(reader.result), 
+		                headers:{'Content-Type': 'application/json; charset=utf-8'}
+		            })    
+		            .then(() => {            
+		                axios.get("http://localhost:8081/WebApartmani/rest/image/base64").then(
+		                		(response) => this.src = response.data)
+		            }) }
+			}
+		},
+		change : function(event) {
+			this.files = event.target.files;
+		}
 	}
 });
--- WebApartmani/src/images/ImageTestService.java
+++ WebApartmani/src/images/ImageTestService.java
@@ -6,10 +6,15 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("image")
@@ -36,7 +41,7 @@ public class ImageTestService {
 		else
 			return Response.noContent().build();
 	}
-	
+
 	@GET
 	@Path("/2/{key}")
 	@Produces("image/jpg")
@@ -50,20 +55,72 @@ public class ImageTestService {
 			e.printStackTrace();
 		}
 
-	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    try {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
 			ImageIO.write(image, "jpg", baos);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    byte[] imageData = baos.toByteArray();
+		byte[] imageData = baos.toByteArray();
 
-	    // uncomment line below to send non-streamed
-	    return Response.ok(imageData).build();
+		// uncomment line below to send non-streamed
+		return Response.ok(imageData).build();
 
-	    // uncomment line below to send streamed
-	    //return Response.ok(new ByteArrayInputStream(imageData)).build();
+		// uncomment line below to send streamed
+		// return Response.ok(new ByteArrayInputStream(imageData)).build();
 	}
+
+	@POST
+	@Path("base64")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void base64upload(String image, @Context ServletContext context) {
+		if (context.getAttribute("base64test") == null) {
+			context.setAttribute("base64test", image);
+			System.out.println("here");
+		}
+
+	}
+
+	@GET
+	@Path("base64")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String base64download(@Context ServletContext context) {
+		return (String) context.getAttribute("base64test");
+	}
+
+	/*
+	 * @POST
+	 * 
+	 * @Path("/image")
+	 * 
+	 * @Consumes(MediaType.MULTIPART_FORM_DATA)
+	 * 
+	 * @Produces(MediaType.APPLICATION_JSON) public Response uploadImage(
+	 * 
+	 * @FormDataParam("file") InputStream uploadedInputStream,
+	 * 
+	 * @FormDataParam("file") FormDataContentDisposition fileDetails) {
+	 * 
+	 * System.out.println(fileDetails.getFileName());
+	 * 
+	 * String uploadedFileLocation = "/Users/temp/" + fileDetails.getFileName();
+	 * 
+	 * // save it writeToFile(uploadedInputStream, uploadedFileLocation);
+	 * 
+	 * String output = "File uploaded to : " + uploadedFileLocation;
+	 * 
+	 * return Response.ok().build(); }
+	 */
+
+	/*
+	 * private void writeToFile(InputStream uploadedInputStream, String
+	 * uploadedFileLocation) { try { OutputStream out = new FileOutputStream(new
+	 * File(uploadedFileLocation)); int read = 0; byte[] bytes = new byte[1024];
+	 * 
+	 * out = new FileOutputStream(new File(uploadedFileLocation)); while ((read =
+	 * uploadedInputStream.read(bytes)) != -1) { out.write(bytes, 0, read); }
+	 * out.flush(); out.close(); } catch (IOException e) { e.printStackTrace(); } }
+	 */
 
 }
